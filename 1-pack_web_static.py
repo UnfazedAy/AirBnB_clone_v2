0d074cee7f8a3b83d9038b8af2eed15cf5c3d70c
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-29 00:53:23 -0700

    Done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-"""A module for Fabric script that generates a .tgz archive."""
-import os
+"""
+A Fabric script that generates a .tgz archive
+from the contents of the web_static folder of my AirBnB Clone repo,
+using the function do_pack.
+"""
+
+from fabric.api import local
 from datetime import datetime
-from fabric.api import local, runs_once
+from os.path import isdir
 
 
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        d_time.year,
-        d_time.month,
-        d_time.day,
-        d_time.hour,
-        d_time.minute,
-        d_time.second
-    )
+    """Generates a .tgz file"""
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
+        date = datetime.now()
+        current_date = date.strftime("%y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        archived_path = (f"versions/web_static_{current_date}.tgz")
+        local(f"tar -cvzf {archived_path} web_static")
+        return archived_path
     except Exception:
-        output = None
-    return output
+        return None
