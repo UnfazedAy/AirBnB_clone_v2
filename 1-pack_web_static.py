db9d114c588b5b3371d7988c7cc7b1af011d545a
UnfazedAy <ayomidesoniyi@gmail.com>
2022-10-29 00:38:23 -0700

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,22 +8,16 @@ from fabric.api import local, runs_once
 @runs_once
 def do_pack():
     """Archives the static files."""
-    if not os.path.isdir("versions"):
+    if os.path.isdir("versions") is False:
         os.mkdir("versions")
     d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        d_time.year,
-        d_time.month,
-        d_time.day,
-        d_time.hour,
-        d_time.minute,
-        d_time.second
-    )
+    current_date = d_time.strftime("%y%m%d%H%M%S")
+    output = (f"versions/web_static_{current_date}.tgz")
     try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
+        print(f"Packing web_static to {output}")
+        local(f"tar -cvzf {output} web_static")
         size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
+        print(f"web_static packed: {output} -> {size} Bytes")
     except Exception:
         output = None
     return output
