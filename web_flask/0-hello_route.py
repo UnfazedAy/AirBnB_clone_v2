58df07ffe0a7c7cc28586eb2c4418a85d0a396c5
UnfazedAy <ayomidesoniyi@gmail.com>
2022-11-12 14:47:40 +0100

    A script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,10 +10,10 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_page():
     """
-    display “Hello HBNB!”
+    Displays “Hello HBNB!”
     """
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
