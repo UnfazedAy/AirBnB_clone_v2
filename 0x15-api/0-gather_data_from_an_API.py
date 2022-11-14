9b95dbbb2cc924da49eef811bdddfc152ef0c256
UnfazedAy <ayomidesoniyi@gmail.com>
2022-11-14 15:56:51 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,7 +8,7 @@ import json
 import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
